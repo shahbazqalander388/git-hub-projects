669f1273c40ad0e5e6f9c41bd6e963d2965fe626
shahbazqalander388 <syedshahbaz2004@gmail.com>
2025-07-03 09:51:27 -0700

    array method

---
+++ arraymethods.js
@@ -0,0 +1,36 @@
+// // let array=[1,2,3,4,5,6,7,8,9];
+// // console.log(array);
+// // let newarray=array.map((value,index,array)=>{
+// //     return value*2
+
+// // })
+// // console.log(newarray)
+// // task 2
+// let array=["banana","mango","cherry"]
+ 
+// console.log(array);
+
+// let new_array = array.map(arr => arr.toUpperCase())
+
+// console.log(new_array);
+
+
+//  let fruits = ["apple", "banana", "mango"];
+
+// let upperFruits = fruits.map(fruit => fruit.toUpperCase());
+
+// console.log(upperFruits);
+// Output: ["APPLE", "BANANA", "MANGO"]
+// let array=[obj={
+//     name:"shahbaz",age:"23",
+//     name1:"qalander",age:"23",
+    
+// }];
+// console.log(array);
+// let newarray=array.map((obj)=>{
+//     console.log(obj.name.name1);
+    
+//     console.log(obj.name)
+//     console.log(obj.name1)
+// })
+// console.log(newarray);
